87ec4fae60ddf97080d5343cdec48717a3926e84
ersultan <example@gmail.com>
2021-03-21 13:51:53 +0600

    food

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () =>{
+
+    const tabs = document.querySelectorAll('.tabheader__items'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+        function hideTabContent(){
+            tabsContent.forEach(item =>{
+               item.classList.add('hide');
+               item.classList.remove('show');
+            });
+            tabs.forEach(item =>{
+                item.classList.remove('tabheader__item_active');
+            });
+        }
+
+        function showTabContent(i = 0){
+            tabsContent[i].classlist.add('show');
+            tabsContent[i].classlistr.remove('hide');
+            tabs[i].classList.add('tabheader__item_active');
+        }
+        
+
+        hideTabContent();
+        showTabContent();
+
+        tabsParent.addEventListener('click', (event)=>{
+            const target = event.target;
+
+            if (target && target.classlist.contains('tabheader__item')){
+                tabs.forEach((item , i)=>{
+                    if(target == item){
+                        hideTabContent();
+                        showTabContent(i);
+                    }
+                });
+            }
+        });
+});
